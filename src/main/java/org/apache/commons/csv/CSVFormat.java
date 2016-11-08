41864cfb59ddad4b0837ae055b404f5db1754fb4
shadykandeel <shady.kandeel.h@gmail.com>
2016-11-08 13:20:48 +0200

    Update CSVFormat.java

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -426,7 +426,7 @@ public final class CSVFormat implements Serializable {
         this.headerComments = toStringArray(headerComments);
         this.header = header == null ? null : header.clone();
         this.skipHeaderRecord = skipHeaderRecord;
-        validate();
+        //validate();
     }
 
     private String[] toStringArray(final Object[] values) {
