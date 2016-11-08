85816fec4f8602733a972955a53b848cce4045df
shadykandeel <shady.kandeel.h@gmail.com>
2016-11-08 11:53:49 +0200

    Update CSVRecordTest.java

--- src/test/java/org/apache/commons/csv/CSVRecordTest.java
+++ src/test/java/org/apache/commons/csv/CSVRecordTest.java
@@ -173,8 +173,9 @@ public class CSVRecordTest {
         // do:
 		System.out.println("15-1");
          CSVPrinter printer = null;
+		 StringBuilder st = new StrinBuilder();
 	    System.out.println("15-1-1");
-		 printer = new CSVPrinter(null, CSVFormat.DEFAULT);
+		 printer = new CSVPrinter(st, CSVFormat.DEFAULT);
 		System.out.println("15-2");
         final Map<String, String> map = recordWithHeader.toMap();
 		System.out.println("15-3");
